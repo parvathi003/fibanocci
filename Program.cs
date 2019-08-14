431a799e04239103d212f6d7b716756ee6744ab3
parvathi003 <51682284+parvathi003@users.noreply.github.com>
2019-08-14 14:25:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fibonocci
+{
+   public class Fibonacci
+   {  
+     public static void Main(string[] args)  
+      {  
+         int a=0,b=1,c,i,number;    
+         Console.Write("Enter the number of elements: ");    
+         number = int.Parse(Console.ReadLine());  
+         Console.Write(a+" "+b+" "); //printing 0 and 1    
+         for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+         {    
+          c=a+b;    
+          Console.Write(c+" ");    
+          a=b;    
+          b=c;    
+         }    
+      }  
+   }  
+}
